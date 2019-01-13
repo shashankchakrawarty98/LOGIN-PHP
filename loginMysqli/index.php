9f84d7967ec7d31e311bf345fb96f2ad82c8bb59
shashankchakrawarty98 <30284220+shashankchakrawarty98@users.noreply.github.com>
2019-01-13 20:15:26 +0530

    Update index.php

--- loginMysqli/index.php
+++ loginMysqli/index.php
@@ -28,7 +28,7 @@
 			$username = mysqli_real_escape_string($con, $_POST['user']);
 			$password = mysqli_real_escape_string($con, $_POST['pass']);
 			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+			$query 		= mysqli_query($con, "SELECT * FROM employee WHERE  password='$password' and username='$username'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
